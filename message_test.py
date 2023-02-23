80dcf2351b2db4794e61db8ed1937a4a799cada5
koaf <76429102+koaf@users.noreply.github.com>
2023-02-23 22:11:15 +0900

    Update message_test.py

--- message_test.py
+++ message_test.py
@@ -1,10 +1,10 @@
 from pytive import Pytive, CommentType
 
 def message_test():
-    live_id = 'f352rQVtAfY58XPv_lyTuA'
+    live_id = 'lliveid'
 
     client = Pytive()
-    client.login('otpU8lo9_A7zNCGQ8dYf4Myy0VDdngGummbJg_okIZto2Rlo-VrolzFxoNtl-d8J', 'f352rQVtAfY58XPv_lyTuA')
+    client.login('mid', 'f')
 
     client.join_live(live_id)
     client.comment(live_id, CommentType.NORMAL, 'pog')
