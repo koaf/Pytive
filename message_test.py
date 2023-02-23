277a5dc4f9202221c4a3b985d97cd75af60d8034
koaf <taichiro_nishi@icloud.com>
2023-02-23 22:12:43 +0900

    2

--- message_test.py
+++ message_test.py
@@ -1,13 +1,18 @@
 from pytive import Pytive, CommentType
-
+n = 1
 def message_test():
     live_id = 'lliveid'
 
     client = Pytive()
     client.login('mid', 'f')
+    while n == 1:
+        live_id = 'liveid'
 
-    client.join_live(live_id)
-    client.comment(live_id, CommentType.NORMAL, 'pog')
+        client = Pytive()
+        client.login('mid', 'f')
+
+        client.join_live(live_id)
+        client.comment(live_id, CommentType.NORMAL, 'pog')
 
 if __name__ == '__main__':
     message_test()
